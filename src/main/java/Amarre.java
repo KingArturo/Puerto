b1efc85ecc74311eefd177aaab4d914932bad375
Arturo <arturomonjerobles@gmail.com>
2020-05-14 13:41:37 +0200

    Commit01 La clase Amarre inicializa sus variable y puede devolver el valor de estás

--- src/main/java/Amarre.java
+++ src/main/java/Amarre.java
@@ -1,2 +1,35 @@
 public class Amarre {
+
+    //Número identificativo del amarre
+    private int numAmarre;
+    //Cliente que alquila el amarre
+    private Cliente client;
+    //Barco que esta en el amarre
+    private Barco barco;
+    //Número de días que se ha alquilado
+    private int diasAlquiler;
+
+    public Amarre(int numAmarre) {
+        this.numAmarre = numAmarre;
+        client = null;
+        barco = null;
+        diasAlquiler = 0;
+    }
+
+    public int getNumAmarre() {
+        return numAmarre;
+    }
+
+    public Cliente getClient() {
+        return client;
+    }
+
+    public Barco getBarco() {
+        return barco;
+    }
+
+    public int getDiasAlquiler() {
+        return diasAlquiler;
+    }
+    
 }
